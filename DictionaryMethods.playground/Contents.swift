122521c3d6b120a5925ceaf97f0dd9a246e55831
Missy Allan <missyallan@Melissas-Air.fios-router.home>
2016-09-18 18:50:09 -0400

    dictionary method read me done

--- DictionaryMethods.playground/Contents.swift
+++ DictionaryMethods.playground/Contents.swift
@@ -72,7 +72,7 @@ if var films = movies["The Fast and the Furious"] {
 }
 
 if let fastMovies = movies["The Fast and the Furious"] {
-    for (index, film) in fastMovies.enumerate() {
+    for (index, film) in fastMovies.enumerated() {
         print("\(index + 1). \(film)")
     }
 }
@@ -89,7 +89,7 @@ if let fast3rFilms = movies["The Fast and the Furious"] {
     print("YAY! no more Fast & Furious movies!")
 }
 
-movies.removeValueForKey("The Fast and the Furious")
+
 
 
 
